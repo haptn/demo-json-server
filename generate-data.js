a45a48ac2961f602b7af7ea35471374026bd5c39
Pham Thi Ngoc Ha <gauchocun@gmail.com>
2023-01-22 20:38:24 +0700

    Recover lost code (due to push -f without merging):
    - Add port config for CI/CD
    - Add functions generate emails from names & generate statuses/roles/...

--- generate-data.js
+++ generate-data.js
@@ -6,6 +6,52 @@ import fs from 'fs'
 // Set locale to use Vietnamese
 faker.locale = 'vi'
 
+<<<<<<< HEAD
+=======
+const _convertVNToEn = (strVN = '') => {
+  let str = strVN
+  str = str.toLowerCase()
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, 'a')
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, 'e')
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, 'i')
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, 'o')
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, 'u')
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, 'y')
+  str = str.replace(/đ|Đ|ð/g, 'd')
+  return str
+}
+
+const _generateEmail = (name = '') => {
+  const arr = name?.split(' ')
+  const lastName = _convertVNToEn(arr[arr?.length - 1]?.toLowerCase())
+  let shortName = lastName
+
+  shortName += arr?.map(item => _convertVNToEn(item?.toLowerCase())
+    ?.slice(0, 1))?.join('')
+  shortName = shortName?.substring(0, shortName?.length - 1)
+
+  return shortName + '@aty.edu.vn'
+}
+
+const _generateSchoolStatus = () => {
+  const status = Math.round(Math.random() * 2)
+  return status === 1 ? 'Đang hoạt động'
+    : status === 2 ? 'Sắp hoạt động' : 'Tạm đóng'
+}
+
+const _generateStaffStatus = () => {
+  const status = Math.round(Math.random())
+  return status === 1 ? 'Đang làm' : 'Đã nghỉ'
+}
+
+const _generateRole = () => {
+  const roleId = Math.round(Math.random() * 10)
+
+  return roleId === 1 ? 'Admin trường'
+    : roleId === 2 ? 'Admin ATY' : 'Kế toán'
+}
+
+>>>>>>> d049f209db3899579ff9409a134fa0e6c34fa4e0
 const randomSchoolsList = numOfSchools => {
   if (numOfSchools <= 0) return []
 
@@ -255,15 +301,19 @@ const randomAccountList = (schoolsList, numOfAccounts) => {
 
   for (const school of schoolsList) {
     Array.from(new Array(numOfAccounts)).forEach(() => {
+      const name = faker.name.lastName() + ' ' + faker.name.firstName()
+
       const account = {
         schoolId: school?.id,
         id: faker.datatype.uuid(),
-        name: faker.name.lastName() + ' ' + faker.name.firstName(),
-        email: faker.internet.email(),
+        name,
+        email: _generateEmail(name),
+        phone: faker.phone.number('0#########'),
+        role: _generateRole(),
         password: faker.datatype.uuid().slice(0, 6),
         avatar: faker.image.avatar(),
         address: faker.address.streetAddress() + ', ' + faker.address.stateAbbr(),
-        status: faker.datatype.boolean(),
+        status: _generateStaffStatus(),
         createdAt: Date.now(),
         updatedAt: Date.now()
       }
